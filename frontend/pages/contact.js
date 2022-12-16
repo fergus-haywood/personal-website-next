36cdc9c729477a2442de0fc329e3f61abb2fa29a
Fergus  Haywood <fergus.haywood5@gmail.com>
2022-12-16 15:45:12 +1300

    add success and error message to contact form

--- frontend/pages/contact.js
+++ frontend/pages/contact.js
@@ -2,21 +2,74 @@ import { useState } from 'react'
 import Layout from '../components/Layout'
 
 export default function Contact(props) {
+  // States for contact form fields
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [message, setMessage] = useState('')
 
-  const handleSubmit = (e) => {
+  //   Form validation state
+  const [errors, setErrors] = useState({})
+
+  //   Setting button text on form submission
+  const [buttonText, setButtonText] = useState('Send')
+
+  // Setting success or failure messages states
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+  const [showFailureMessage, setShowFailureMessage] = useState(false)
+
+  // Validation check method
+  const handleValidation = () => {
+    let tempErrors = {}
+    let isValid = true
+
+    if (name.length <= 0) {
+      tempErrors['fullname'] = true
+      isValid = false
+    }
+    if (email.length <= 0) {
+      tempErrors['email'] = true
+      isValid = false
+    }
+    if (message.length <= 0) {
+      tempErrors['message'] = true
+      isValid = false
+    }
+
+    setErrors({ ...tempErrors })
+    console.log('errors', errors)
+    return isValid
+  }
+
+  const handleSubmit = async (e) => {
     e.preventDefault()
+
+    let isValidForm = handleValidation()
+
     const data = {
       name,
       email,
       message,
     }
-    fetch('/api/contact', {
-      method: 'post',
-      body: JSON.stringify(data),
-    })
+
+    if (isValidForm) {
+      setButtonText('Sending')
+      const res = await fetch('/api/contact', {
+        method: 'post',
+        body: JSON.stringify(data),
+      })
+      console.log(res)
+      const { error } = await res.json()
+      if (error) {
+        console.log(error)
+        setShowSuccessMessage(false)
+        setShowFailureMessage(true)
+        setButtonText('Send')
+        return
+      }
+      setShowSuccessMessage(true)
+      setShowFailureMessage(false)
+      setButtonText('Sent')
+    }
   }
 
   return (
@@ -47,10 +100,28 @@ export default function Contact(props) {
             rows="4"
             onChange={(e) => setMessage(e.target.value)}
           />
-          <button className="projectLink" type="submit">
-            Send{' '}
-            <img className="form-send-arrow" src="./sendArrow.svg" width="20" />
-          </button>
+          {!showSuccessMessage && (
+            <button className="projectLink" type="submit">
+              {buttonText}{' '}
+              <img
+                className="form-send-arrow"
+                src="./sendArrow.svg"
+                width="20"
+              />
+            </button>
+          )}
+          <div className="contact-form__message">
+            {showSuccessMessage && (
+              <p>Thanks for your message, i'll get back to you shortly</p>
+            )}
+            {showFailureMessage && (
+              <p>
+                Sorry there was an error, please{' '}
+                <a href="mailto:hello@fergushaywood.com">email me</a> instead at
+                <a href="mailto:hello@fergushaywood.com">hello@fergushaywood</a>
+              </p>
+            )}
+          </div>
         </form>
       </div>
     </Layout>
