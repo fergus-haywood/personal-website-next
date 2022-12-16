db6810379cef51b0acea7065447b8472f30ccd26
Fergus  Haywood <fergus.haywood5@gmail.com>
2022-12-16 15:45:12 +1300

    add success and error message to contact form

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dd7400f9da126a9b"; }
+/******/ 		__webpack_require__.h = function() { return "44ad09d7ba7eaf28"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
