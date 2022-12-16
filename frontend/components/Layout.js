460bccf6cd84a0b9cb4ab5e16159b093d619518a
Fergus  Haywood <fergus.haywood5@gmail.com>
2022-12-17 09:17:32 +1300

    rename header and footer in import statement

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
-import Header from './header'
-import Footer from './footer'
+import Header from './Header'
+import Footer from './Footer'
 import Navigation from './Navigation'
 
 export default function Layout(props) {
