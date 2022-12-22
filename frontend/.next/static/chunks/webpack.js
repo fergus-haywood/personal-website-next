bcc581aab550b3e966924cb545b6294f85662a71
Fergus  Haywood <fergus.haywood5@gmail.com>
2022-12-22 17:10:42 +1300

    add margin on bottom of p

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e6d25a81701dd692"; }
+/******/ 		__webpack_require__.h = function() { return "df250bd79f386914"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
