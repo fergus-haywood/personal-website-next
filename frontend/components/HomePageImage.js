5fe7e0f19ae527e427e40b45c399fef03ef52131
Fergus  Haywood <fergus.haywood5@gmail.com>
2022-12-22 13:07:01 +1300

    add image to the homepage

--- frontend/components/HomePageImage.js
+++ frontend/components/HomePageImage.js
@@ -1,9 +1,30 @@
-import styles from '../styles/HomePageImage.modules.css'
+import styles from '../styles/HomePageImage.module.css'
+import imageUrlBuilder from '@sanity/image-url'
+import Image from 'next/image'
 
 export default function HomePageImage({ image }) {
+  const homepageImage = image.result[0][0].image
+  console.log(homepageImage)
+
+  function urlFor(source) {
+    return imgBuilder.image(source)
+  }
+
+  const imgBuilder = imageUrlBuilder({
+    projectId: '36om7i3d',
+    dataset: 'production',
+  })
+
   return (
     <div className={styles.wrapper}>
-      <h1>Testing</h1>
+      <Image
+        src={urlFor(homepageImage).width(1500).quality(100).url()}
+        className={styles.image}
+        layout="fill"
+        alt="Fergus Haywood Homepage Image"
+        placeholder="blur"
+        blurDataURL={urlFor(homepageImage).url()}
+      />
     </div>
   )
 }
