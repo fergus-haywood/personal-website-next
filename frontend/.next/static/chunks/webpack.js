b08b7f9796451a6616f8f9f80a252cfa533261fb
Fergus  Haywood <fergus.haywood5@gmail.com>
2022-12-22 17:33:21 +1300

    adjust project disciplines color on border

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "35f8d1634b8a3291"; }
+/******/ 		__webpack_require__.h = function() { return "20813af8033eac4c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
