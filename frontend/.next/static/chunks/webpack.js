d2dfa4fbdbfc32e12bb325e67c89ca12c9fe049b
Fergus  Haywood <fergus.haywood5@gmail.com>
2022-12-23 16:54:47 +1300

    add working navigation

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0f3b5013c0dc5d57"; }
+/******/ 		__webpack_require__.h = function() { return "c4f4bbd133be45a8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
