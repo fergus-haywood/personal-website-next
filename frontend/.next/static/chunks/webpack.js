b0fe66d2baf088efbd2462ef31d0dc54c5095bcc
Fergus  Haywood <fergus.haywood5@gmail.com>
2022-12-23 17:25:17 +1300

    trying to add .next to gitignore

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2687895171487aad"; }
+/******/ 		__webpack_require__.h = function() { return "a08d77ff255d977e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
