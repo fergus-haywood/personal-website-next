76485711d0a00b0d678f8cbc1955989d60f24a20
Fergus  Haywood <fergus.haywood5@gmail.com>
2023-01-09 11:10:57 +1300

    launch ga with env variables

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -7,17 +7,21 @@ export default function MyApp(props) {
   const { Component, pageProps } = props
   return (
     <>
-      {/* <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-32CBED8Z0C"
-        strategy="afterInteractive"
-      ></Script>
-      <Script>
-        window.dataLayer = window.dataLayer || []; function gtag()
-        {dataLayer.push(arguments)}
-        gtag('js', new Date()); gtag('config', 'G-32CBED8Z0C');
-      </Script> */}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
 
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
       <Layout navigation={props.navigationBody}>
         <Component {...pageProps} />
       </Layout>
