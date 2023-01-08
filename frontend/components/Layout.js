e8b1b6fc3a11f1e334e9d2ec20a8ac8ca2ef04e9
Fergus  Haywood <fergus.haywood5@gmail.com>
2023-01-09 10:49:33 +1300

    remove google tag from layout

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -2,8 +2,6 @@ import Head from 'next/head'
 import Header from './Header'
 import Footer from './Footer'
 import Navigation from './Navigation'
-import { useEffect, useState } from 'react'
-import Script from 'next/script'
 
 export default function Layout(props) {
   return (
@@ -15,16 +13,6 @@ export default function Layout(props) {
           content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=0"
         />
       </Head>
-
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-32CBED8Z0C"
-      ></Script>
-      <Script>
-        window.dataLayer = window.dataLayer || []; function gtag()
-        {dataLayer.push(arguments)}
-        gtag('js', new Date()); gtag('config', 'G-32CBED8Z0C');
-      </Script>
 
       <Navigation content={props.navigation} />
       <Header content={props.navigation} />
