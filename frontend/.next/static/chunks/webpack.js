9d13da3dde9c8bf532e4c3e02749e4fdc2f7736b
Fergus  Haywood <fergus.haywood5@gmail.com>
2023-01-12 12:11:07 +1300

    re-install next and typescript

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "af2b505735e48dc1"; }
+/******/ 		__webpack_require__.h = function() { return "9bc39d8176efa4c9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
