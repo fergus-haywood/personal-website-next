6d04612672c0f1d78fd8e39f7327aea420e59696
Fergus  Haywood <fergus.haywood5@gmail.com>
2023-01-12 13:45:52 +1300

    initial commit

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "75dde07200da270f"; }
+/******/ 		__webpack_require__.h = function() { return "813fee90c0d7f13f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/pages/clients.js
+++ frontend/pages/clients.js
@@ -103,7 +103,7 @@ export default function Clients(props) {
   )
 }
 
-export async function getServerSideProps() {
+export async function getInitialProps() {
   const navigationQuery = encodeURIComponent(`*[ _type == 'navigation']`)
   const navigationURL = `https://36om7i3d.api.sanity.io/v1/data/query/production?query=[${navigationQuery}]`
   const navigationBody = await fetch(navigationURL).then((res) => res.json())
