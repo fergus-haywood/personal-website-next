bd716058ff3fd94374bc450eb3095fc6c6740495
Fergus  Haywood <fergus.haywood5@gmail.com>
2023-01-12 13:37:22 +1300

    re-added nprogress

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c556bb29bf0db8de"; }
+/******/ 		__webpack_require__.h = function() { return "75dde07200da270f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
