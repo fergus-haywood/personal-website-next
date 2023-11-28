96815b29d2341529a5f57c9cc4a04547d4b08b13
Fergus  Haywood <fergus.haywood5@gmail.com>
2023-11-28 19:44:33 +1300

    remove semicolon

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -5,28 +5,29 @@ import Navigation from './Navigation'
 import Script from 'next/script'
 
 export default function Layout(props) {
-  return (
-    <>
-      <Head>
-        <title>Fergus Haywood</title>
-        <meta
-          name="viewport"
-          content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=0"
-        />
+	return (
+		<>
+			<Head>
+				<title>Fergus Haywood</title>
+				<meta
+					name="viewport"
+					content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=0"
+				/>
 
-<Script async src="https://www.googletagmanager.com/gtag/js?id=G-32CBED8Z0C"></Script>
-<Script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-32CBED8Z0C');
-</Script>
-      </Head>
-      <Header props={props} />
-      <Navigation content={props} />
-      <main className="main-wrapper">{props.children}</main>
-      <Footer />
-    </>
-  )
+				<Script
+					async
+					src="https://www.googletagmanager.com/gtag/js?id=G-32CBED8Z0C"
+				></Script>
+				<Script>
+					window.dataLayer = window.dataLayer || []; function gtag()
+					{dataLayer.push(arguments)}
+					gtag('js', new Date()); gtag('config', 'G-32CBED8Z0C');
+				</Script>
+			</Head>
+			<Header props={props} />
+			<Navigation content={props} />
+			<main className="main-wrapper">{props.children}</main>
+			<Footer />
+		</>
+	)
 }
